1c38a618b69133be8ad226cd97d9526950501ec0
Marcelino <marcelinokenny12@gmail.com>
2025-01-16 21:30:32 +0700

    Add car brands and types data with associated images

---
+++ src/app/lib/data.ts
@@ -0,0 +1,69 @@
+export interface BrandProps {
+  name: string;
+  image: string;
+}
+
+export const carBrands: BrandProps[] = [
+  {
+    name: "Mercedes Benz",
+    image: "/ic_mercedes.svg"
+  },
+  {
+    name: "BMW",
+    image: "/ic_bmw.svg"
+  },
+  {
+    name: "Audi",
+    image: "/ic_audi.svg"
+  },  {
+    name: "Toyota",
+    image: "/ic_toyota.svg"
+  },
+  {
+    name: "Honda",
+    image: "/ic_honda.svg"
+  },
+  {
+    name: "Tesla",
+    image: "/ic_tesla.svg"
+  },
+  {
+    name: "Hyundai",
+    image: "/ic_hyundai.svg"
+  },
+  {
+    name: "Nissan",
+    image: "/ic_nissan.svg"
+  },
+  {
+    name: "Volkswagen",
+    image: "/ic_volkswagen.svg"
+  },
+  {
+    name: "Ford",
+    image: "/ic_ford.svg"
+  },
+  {
+    name: "KIA",
+    image: "/ic_kia.svg"
+  },
+    {
+    name: "Mitsubishi",
+    image: "/ic_mitsubishi.svg"
+  },
+];
+
+export const carTypes: BrandProps[] = [
+  {
+    name: "Sedan",
+    image: "/body_sedan.svg"
+  },
+  {
+    name: "SUV",
+    image: "/body_suv.svg"
+  },
+  {
+    name: "Coupe",
+    image: "/body_coupe.svg"
+  },
+];
